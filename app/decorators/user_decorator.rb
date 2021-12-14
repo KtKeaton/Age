80a9231a1e15722b935fce63d3ecf66f529264bb
KtKeaton <ktr01@protonmail.com>
2021-12-14 15:50:32 +0800

    UserDecorator修正為delegate_all

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,12 +1,16 @@
 class UserDecorator < Draper::Decorator
   include Draper::LazyHelpers
-  decorates_association :user
+  delegate_all
 
   # def date
   #   model.date
   # end
 
   def birthday(date)
+    "民國#{date.year - 1911}年#{date.month}月#{date.day}日"
+  end
+
+  def birthday_sino(date)
     "民國#{date.year - 1911}年#{date.month}月#{date.day}日"
   end
 
